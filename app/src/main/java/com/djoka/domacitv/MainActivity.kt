30863b519862865a86823af4bf6a2cf04474bd58
Igor123754 <igormiladinovic3@gmail.com>
2026-08-08 04:09:06 +0200

    Add navigation between Home and Detail screens

--- app/src/main/java/com/djoka/domacitv/MainActivity.kt
+++ app/src/main/java/com/djoka/domacitv/MainActivity.kt
@@ -8,6 +8,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.djoka.domacitv.ui.DetailScreen
 import com.djoka.domacitv.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +31,29 @@ fun DomaciTVApp() {
         modifier = Modifier.fillMaxSize(),
         color = Color.Black
     ) {
-        HomeScreen()
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") {
+                HomeScreen(onItemClick = { type, id ->
+                    navController.navigate("detail/$type/$id")
+                })
+            }
+            composable(
+                route = "detail/{type}/{id}",
+                arguments = listOf(
+                    navArgument("type") { type = NavType.StringType },
+                    navArgument("id") { type = NavType.StringType }
+                )
+            ) { backStackEntry ->
+                val type = backStackEntry.arguments?.getString("type") ?: ""
+                val id = backStackEntry.arguments?.getString("id") ?: ""
+                DetailScreen(
+                    type = type,
+                    id = id,
+                    onBack = { navController.popBackStack() }
+                )
+            }
+        }
     }
 }
