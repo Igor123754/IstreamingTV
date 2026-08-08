8aa7d75b5244aa55ccc8be8b88e82808cada8879
Igor123754 <igormiladinovic3@gmail.com>
2026-08-08 05:11:06 +0200

    Add ExoPlayer dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-exoplayer-hls:1.4.1")
+    implementation("androidx.media3:media3-exoplayer-dash:1.4.1")
+    implementation("androidx.media3:media3-ui:1.4.1")
 }
